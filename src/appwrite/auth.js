41507266b9e2b34f2ebefdd263c01836ef15cfee
Maheen Fatma <goodmaheen@gmail.com>
2024-07-22 00:19:34 +0530

    Appwrite auth : a future proof code for appwrite. This file can be reused in every appwrite project which requires auth services as it contains every auth related method - signup, signin, signout etc. This also prevents us from vendor lockin. We can change the backend from appwrite to firebase to any other service from within this file without affecting the frontend portion.

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -9,10 +9,10 @@ export class AuthService {
         this.account = new Account(this.client);
      }
 
-     async createAccount ({email, password, name}) 
+     async createAccount ({email, password, name})  // this is the funcation that needs to be called at the time signup
      {
         try {
-            const user = await account.create(
+            const user = await this.account.create(
                 ID.unique(), 
                 email, 
                 password,
@@ -21,7 +21,8 @@ export class AuthService {
             
             if(user) //the user might not be able to complete auth due to some reason
             {
-                //
+                //if the user has signed in, we will automatically log him
+                return this.login({email, password})
             }
             else
             {
@@ -33,8 +34,42 @@ export class AuthService {
             throw(error)
         }
      }
+
+     async login ({email, password})
+     {
+        try {
+            const session = await this.account.createEmailSession(
+                email, 
+                password
+            );
+            return session;
+            
+        } catch (error) {
+            throw(error)
+        }
+     }
+
+     //we also need to check if the user is logged in or not for uses in various components or pages. "This function will be used in our components and routes to check if a user is logged in, and access the user's details".
+    async  getLoggedInUser() {
+        try {
+          
+          return await this.account.get();
+        } catch (error) { //catch is executed if we are not able to reach out to a service
+          throw(error);
+        }
+        return null; //if try doesnot give us anything
+      }
+
+    async logout (){
+        try {
+             await this.account.deleteSessions( );
+            
+        } catch (error) {
+            throw(error);
+        }
+    }
 }
 
-const authService = new AuthService();
+const authService = new AuthService(); //so that we can simply use each services as authService.login(...) etc.
 
 export default authService;
