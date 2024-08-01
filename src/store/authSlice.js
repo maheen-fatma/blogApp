c1c559283974a9294d9e3bb3fe77e5dabcf6e7eb
Maheen Fatma <goodmaheen@gmail.com>
2024-08-02 00:49:58 +0530

    Create auth slice to store if any user is logged in or not

---
+++ src/store/authSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+//this slice is created to store if there is any user who has logged in or not and if yes, then the details of the user
+//this will help give the various accesibility rights to the user who has loggen in
+const intialState= {
+    status: false,
+    userInfo: null
+}
+
+export const authSlice = createSlice({
+    name:"auth",
+    intialState,
+    reducer: {
+        login: (state, action) => {
+            state.status= true
+            state.userInfo= action.payload.userInfo
+        },
+        logout : (state) =>{
+            state.status= false
+            state.userInfo= null
+        }
+    }
+})
+
+export const {login, logout}= authSlice.actions
+export default authSlice.reducer
