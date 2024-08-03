00d3fcda6ae277a13af1d378c7945d8502f6f9d6
Maheen Fatma <goodmaheen@gmail.com>
2024-08-03 12:59:07 +0530

    feat: Conditional rendering feature based upon loading state while the current user data is being fetched and dispatched

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import authService from "./appwrite/auth"
+import { login, logout } from "./store/authSlice"
 
 function App() {
-  
 
-  return (
-    <>
-      <h1>This is a blog app by Maheen</h1>
-    </>
+  //a loading state which would be set false when the user is either fetched or not fetched
+  //use loading state to show "Loading..." message while the data is being fetched from appwrite auth service
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+      authService.getLoggedInUser()
+      .then( (userInfor) => { // if there is a user then run the login reducer form the store which causes the login status to be true
+        if(userInfo){
+          dispatch(login({userInfo}))
+        }
+        else {
+          dispatch(logout()) //else false the login sttaus
+        }
+      } )
+      .finally( () =>  //finally after all this, set loading as false to stop showing 
+        setLoading(false)
+      )
+  },[])
+  
+  //conditional rendering as per loading
+  return !loading ? (
+    <div>
+      Blog app data
+    </div>
+  ) : (
+    <div>
+      Loading..
+    </div>
   )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 
-export default {
+export  {
     Header,
     Footer
 }
