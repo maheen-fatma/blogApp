092b490daadfc5c8d18f99449e6cd934edefd0f0
Maheen Fatma <goodmaheen@gmail.com>
2024-08-09 21:07:24 +0530

    Create nav bar items as array of objects with properties - name (display), slug (for routing) and active (to know when to display)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,44 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 function Header() {
-  const isLogin = useSelector((state) => state.auth.status);
-  console.log('isLogin:', isLogin); // Debugging line
+  const isLogin = useSelector((state) => state.auth.status); //this line see the userlogin status
+  
+  //items in the header are created as array of objects
+  const navItems = [
+    {
+      name:'Home',
+      slug:"/",
+      active: true //always visible irrespective of the fact user is logged in or not
+    },{
+      name:'All Posts',
+      slug:"/all-posts",
+      active: isLogin
+    },{
+      name:'My Post',
+      slug:"/my-post",
+      active: isLogin
+    },{
+      name:'Add Post',
+      slug:"/add",
+      active: isLogin
+    },{
+      name:'Sign-in',
+      slug:"/signin",
+      active: !isLogin //show this only when the user is not loggen in
+    },{
+      name:'Sign-Up',
+      slug:"/signup",
+      active: !isLogin
+    },{
+      name:'Home',
+      slug:"/",
+      active: true
+    },
+  ]
   return (
     <div>
       Header
-      log{isLogin}
+      
     </div>
   )
 }
