a21369238726d39be137a4a3fc2362347f43f13d
Maheen Fatma <goodmaheen@gmail.com>
2024-08-13 21:45:58 +0530

    Created preview for a post components, yet to beautify.

---
+++ src/components/PostPreview.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import dbService from '../appwrite/databases'
+function PostPreview({
+    $id, //the id will be used to navigate to the entire post
+    title="Post title",
+    image
+}) {
+  return (
+    <Link to={`\posts\${$id}`}> 
+        <div className=' bg-yellow-300'>
+            <h1>TO BE DESIGNED</h1>
+            <img src={dbService.getFilePreview(image)} alt="Image" />
+            <h1>{title}</h1>
+        </div>
+    </Link>
+  )
+}
+
+export default PostPreview
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { PostPreview } from '../components'
 function Home() {
   return (
     <div>
       Home
+      <PostPreview/>
     </div>
   )
 }
