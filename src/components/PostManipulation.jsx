90760bc716d7a7c2b78d39b2c072015adb9a43e3
Maheen Fatma <goodmaheen@gmail.com>
2024-08-17 21:34:23 +0530

    Set up states for various inputs by the user. Set up the input components for input by the user. handleInputChange function for updating the states

--- src/components/PostManipulation.jsx
+++ src/components/PostManipulation.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-
-function PostManipulation() {
+import React, { useState } from 'react'
+import Input from './Input'
+function PostManipulation({post}) {
+    const [formData, setFormData] = useState({
+        title: post?.title || "",
+        slug: post?.slug || "",
+        content: post?.content || "",
+        image: null
+    })
+    const handleInputChange = (e) => {
+        const [name, value] = e.target;
+        setFormData((prev)=>({
+            ...prev,
+            [name]: value,
+        }))
+    }
   return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+        <Input
+            label= "Title: "
+            name="title"
+            value={formData.title}
+            placeholder= "Title"
+            onChange={handleInputChange}
+            required
+        />
+        <Input
+            label= "Title: "
+            name="title"
+            value={formData.title}
+            placeholder= "Title"
+            onChange={handleInputChange}
+            required
+        />
+        
+      </form>
     </div>
   )
 }
