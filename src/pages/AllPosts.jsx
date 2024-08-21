41697bc3f417fe38e61c95e5cd5a0f9e59d889d0
Maheen Fatma <goodmaheen@gmail.com>
2024-08-21 22:12:51 +0530

    Looped infinite scrolling added

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -2,21 +2,36 @@ import React, { useEffect, useState } from 'react'
 import { PostPreview } from '../components'
 import dbService from '../appwrite/databases'
 import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
+import InfiniteScroll from 'react-infinite-scroll-component'
+
 function AllPosts() {
     const [posts , setPosts] = useState([])
+    const [hasMore, setHasMore] = useState(true)
     useEffect(()=>{
-        dbService.getAllPost()
-        .then((posts)=>{
-            if(posts)
-                setPosts(posts.documents) //the getAllPosts function returns an object with documents as a property which inturn is an array of objects containiing all the info of the posts
+      loadPosts()      
+    },[])
+
+    const loadPosts = () => {
+      dbService.getAllPost()
+        .then((newPosts)=>{
+            if(newPosts && newPosts.documents.length > 0)
+                setPosts((prevPosts) => [...prevPosts, ...newPosts.documents]) //the getAllPosts function returns an object with documents as a property which inturn is an array of objects containiing all the info of the posts
+            else
+                setHasMore(false) // Stop loading when no more posts
         })
         
-        
-    },[])
+    }
   return (
-    <div className=' '>
-      <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 4}}>
-        <Masonry gutter="16px">
+    <div className=' p-[20px] '>
+      <InfiniteScroll
+                dataLength={posts.length}
+                next={loadPosts}
+                hasMore={hasMore}
+                loader={<h4 className=' font-dolce p-3 justify-center'>Loading...</h4>}
+                endMessage={<p>No more posts</p>}
+            >
+      <ResponsiveMasonry columnsCountBreakPoints={{350: 2, 750: 3, 900: 4}}>
+        <Masonry gutter="20px">
       {posts && posts.map((item)=>(
         <div key={item.$id} className=' '>
             <PostPreview {...item} />
@@ -24,6 +39,7 @@ function AllPosts() {
       ))}
         </Masonry>
       </ResponsiveMasonry>
+      </InfiniteScroll>
     </div>
   )
 }
