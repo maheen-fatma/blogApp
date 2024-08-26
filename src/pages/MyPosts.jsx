741090290148f3d092f20db16af0c3b5054f5055
Maheen Fatma <goodmaheen@gmail.com>
2024-08-26 16:05:24 +0530

    MyPost section should only show the post that belongs to the logged in user

--- src/pages/MyPosts.jsx
+++ src/pages/MyPosts.jsx
@@ -2,9 +2,11 @@ import React, { useState , useEffect } from 'react'
 import dbService from '../appwrite/databases'
 import { PostPreview } from '../components'
 import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
+import { useSelector } from 'react-redux'
 
 function MyPosts() {
   const [posts, setPosts] = useState([])
+  const userData = useSelector((state)=>(state.auth.userInfo))
   useEffect(()=>{
     dbService.getAllPost().then((newPosts)=>{
       if(newPosts)
@@ -16,7 +18,10 @@ function MyPosts() {
     <div className=' px-10 p-[20px]  '>
       <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}>
         <Masonry gutter="20px">
-      {posts && posts.map((item)=>(
+      {posts &&  
+      posts
+      .filter((item)=> item.userId === userData.$id)
+      .map((item)=>(
         <div key={item.$id} className=' '>
             <PostPreview {...item} />
         </div>
