402df6b4850d8d29896fc8e03c2c7cae3af0096f
Maheen Fatma <goodmaheen@gmail.com>
2024-08-26 15:15:21 +0530

    Created my posts page

--- src/pages/MyPosts.jsx
+++ src/pages/MyPosts.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React, { useState , useEffect } from 'react'
+import dbService from '../appwrite/databases'
+import { PostPreview } from '../components'
 function MyPosts() {
+  const [posts, setPosts] = useState([])
+  useEffect(()=>{
+    dbService.getAllPost().then((newPosts)=>{
+      if(newPosts)
+        setPosts(newPosts.documents)
+
+    })
+  },[])
   return (
-    <div>
-      hello
+    <div className=' px-10 p-[20px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 space-x-5 space-y-5 '>
+      {posts && posts.map((item)=>( 
+        <div key={item.$id} className=' '>
+            <PostPreview {...item} />
+        </div>
+      ))}
+        
     </div>
   )
 }
