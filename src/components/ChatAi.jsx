38457b16151884bf299145ad4ecde1d70587685c
Maheen Fatma <goodmaheen@gmail.com>
2024-09-03 00:28:53 +0530

    Basic input for taking prompt and receiving the response

--- src/components/ChatAi.jsx
+++ src/components/ChatAi.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import conf from '../conf/conf';
 import { GoogleGenerativeAI } from "@google/generative-ai";
-
+import parse from 'html-react-parser'
 function ChatAi() {
 const genAI = new GoogleGenerativeAI(conf.geminiApiKey);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-
-const prompt = "Write a story about a magic backpack.";
+const [prompt, setPrompt] = useState("")
+const [respons, setRespons] = useState("");
 async function generateResponse(){ 
     try{
         const result = await model.generateContent(prompt);
-        console.log(result.response.text()); // Assuming 'text' is the property containing the generated content
+        setRespons(result.response.text())
+        
     } catch (error) {
         console.error("Error generating content:", error);
     }
 }
+
   return (
     <div>
+      <input 
+        type="text" 
+        name="" 
+        id="" 
+        className='border border-gray-300 bg-fuchsia-300' 
+        onChange={(e)=>setPrompt(e.target.value)}
+        value={prompt} 
+       />
       <button onClick={generateResponse}>
         Click here
       </button>
+      {
+        respons && 
+        <div>
+            <p>{parse(respons)}</p>
+        </div>
+      }
     </div>
   )
 }
